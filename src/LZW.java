8c2fcc8d9ebd11e9c841ce1168ff8dfac55a7771
lanamikalim <47696234+lanamikalim@users.noreply.github.com>
2021-09-13 22:03:40 -0700

    Added decompression
    -added decompress method & additional helper methods, mainly for converting data types and reading in files
    -There was a problem that came up, I wasn't able to deconstruct the specific way that the original compressed to binary(lines 82-87), and as a result the decompression isn't as specific to the byte formatting that occurred.

--- src/LZW.java
+++ src/LZW.java
@@ -2,12 +2,20 @@
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.IntBuffer;
+import java.nio.channels.ByteChannel;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 public class LZW {
-	
+
 	private ArrayList <String>  dictionary = new ArrayList(1030) ; 
 	private String txt; 
 
@@ -34,7 +42,7 @@ public class LZW {
 	public String compress () throws UnsupportedEncodingException, FileNotFoundException, IOException
 	{
 		String output = ""; 
-		int [] compressed = new int [800000]; 
+		int [] compressed = new int [8000000]; 
 		int k = 0; 
 		int i = 0; 
 		int j = 2; 
@@ -43,6 +51,7 @@ public class LZW {
 			if (txt.length() <= i+j && dictionary.contains(txt.substring (i,txt.length())))
 			{
 				output += (txt.substring(i, txt.length()));
+			
 				int comp =  dictionary.indexOf(txt.substring (i,txt.length())); 
 				compressed[k] = comp; 
 				k++; 
@@ -68,16 +77,87 @@ public class LZW {
 			}
 
 		}
-		
+
 		byte [] byteOutput = new byte [2*k]; 
 		for (int x = 0; x < 2*k; x=x+2)
 		{
 			byteOutput[x] = (byte) compressed[x]; 
-			byteOutput[x+1] = (byte)(compressed[x]/256) ; 
+			byteOutput[x+1] = (byte)(compressed[x]/256) ;
+			
 		}
 		Path file = Paths.get("output.byte");
 		Files.write(file, byteOutput);
 		return output; 
 	}
-	
+
+	public byte[] readFileToString(String filename) throws IOException {
+		String decompressedText = "";
+		String uncompressedString;
+		Path fileName = Path.of(filename);
+		byte[] data = Files.readAllBytes(fileName);
+		//uncompressedString = Files.readString(fileName);
+
+		return data;
+
+
+	}
+
+	public int[] convertToIntArray(byte[] byteArray) {
+		int[] intArray = new int[byteArray.length];
+		for(int i = 0; i < byteArray.length; i++) {
+			intArray[i] = (int)byteArray[i];
+		}
+		return intArray;
+	}
+
+	public String byteToBinary(byte[] bytes) {
+
+		StringBuilder sb = new StringBuilder(bytes.length * Byte.SIZE);
+		for( int i = 0; i < Byte.SIZE * bytes.length; i++ )
+			sb.append((bytes[i / Byte.SIZE] << i % Byte.SIZE & 0x80) == 0 ? '0' : '1');
+		return sb.toString();
+	}
+
+
+	public String decompress(int[] compressedInput) {
+
+		int dictionarySize = 256;
+		Map<Integer,String> dictionary = buildDictionaryForDecompression(dictionarySize);
+
+		String word = "" + (char)(int)compressedInput[0];
+
+		StringBuffer result = new StringBuffer(word);
+
+		for (int i = 1; i < compressedInput.length-1; i++) {
+			String entry;
+			int digit = compressedInput[i];
+
+			if (dictionary.containsKey(digit)) {
+				entry =  dictionary.get(digit);
+			}
+			else if (digit == dictionarySize) {
+				entry = word + word.charAt(0);
+			} 
+			else {
+				throw new IllegalArgumentException("Bad compressed digit: " + digit);
+			}
+
+			result.append(entry);
+
+			dictionary.put(dictionarySize++, word + entry.charAt(0));
+
+			word = entry;
+		}
+
+		return result.toString();
+	}
+
+	private Map<Integer,String> buildDictionaryForDecompression(int dictionarySize) {
+		Map<Integer,String> dictionary = new HashMap<Integer,String>();
+		for (int i = 0; i < dictionarySize; i++) {
+			dictionary.put(i, "" + (char)i);
+		}
+		return dictionary;
+	}
+
 }
--- src/TesterLZW.java
+++ src/TesterLZW.java
@@ -1,10 +1,18 @@
 import java.io.FileNotFoundException;
+import java.util.*;
 import java.io.IOException;
+import java.util.stream.Collectors;
 
 public class TesterLZW {
 	public static void main(String [] args) throws IOException 
 	{
-		LZW john = new LZW ("lzw-file2.txt"); 
-	   System.out.print(john.compress()); 
+		LZW john = new LZW ("smalltest.txt"); 
+		byte[] datas= john.readFileToString("output.byte");
+		String compressed = john.compress(); 
+		int[] compressOutput = {97, 97, 98, 257, 256, 98, 261, 0, 0, 0, 0, 0, 0, 0, 0};
+	
+		System.out.println("decomp: " + john.decompress(compressOutput));
+	 
+	   
 	}
 }
